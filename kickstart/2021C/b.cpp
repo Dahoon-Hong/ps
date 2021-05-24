c8408ec5e43935e1a18ab3835bf968656d3abdce
pos9906 <slayerboxr@gmail.com>
2021-05-24 22:31:59 +0900

    fix b

--- kickstart/2021C/b.cpp
+++ kickstart/2021C/b.cpp
@@ -1,49 +1,24 @@
 #include <bits/stdc++.h>
-#define llu unsigned long long
+typedef unsigned long long llu;
 
 using namespace std;
 
-int get_a(int k, int i)
-{
-
-    double x1 = double(k)/double(i);
-    double x2 = -0.5*double(i);
-    double x3 = -0.5;
-    double x= x1+x2+x3;
-    if(x < 0)
-    {
-        return -1;
-    }
-    if(x - int(x) == 0.0)
-    {
-        return int(x);
-    }
-    else
-    {
-        return -1;
-    }
-}
-
-llu check(int a, int b)
-{
-    return b*(b+1)/2 - a*(a+1)/2;
-}
 
 llu solve()
 {
-    int k;
+    llu k;
     cin >> k;
-    int res =0 ;
-    for(int i = 1 ; i <= k ; i++)
+    llu res = 0;
+    for(llu i = 1 ; ; i++)
     {
-        int x=get_a(k, i);
-        if(x != -1)
+        llu s = i*(i+1)/(llu)2;
+        if(k<s)
         {
-            //cout << x << " " << x+i << endl;
-            if(check(x, x+i)==k)
-            {
-                res++;
-            }
+            break;
+        }
+        if((k - s) % i == 0)
+        {
+            res++;
         }
     }
     return res;
